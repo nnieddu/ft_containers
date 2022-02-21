a1a9f64277da5e7128557a338c3deee437925dae
nnieddu <nicolas.nieddu@free.fr>
2022-02-21 09:11:03 +0100

    map begin

---
+++ containers/ft_map.hpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_map.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ninieddu <ninieddu@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/28 10:53:33 by ninieddu          #+#    #+#             */
+/*   Updated: 2022/02/21 09:10:35 by ninieddu         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+// https://www.cplusplus.com/reference/memory/allocator/
+
+// https://www.cplusplus.com/reference/iterator/iterator/ MyIterator
+
+#pragma once
+
+#include <iostream>
+#include <memory>
+
+#include "../utils/iterators/ft_iterator_traits.hpp"
+#include "../utils/iterators/ft_random_access_iterator.hpp"
+
+// template < class Key,                                     // map::key_type
+//            class T,                                       // map::mapped_type
+//            class Compare = less<Key>,                     // map::key_compare
+//            class Alloc = allocator<pair<const Key,T> >    // map::allocator_type
+//            > class map;
+
+//  Maps are typically implemented as binary search trees.
+
+namespace ft 
+{	
+	template < class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator<std::pair<const Key,T> > > //TO CHANGE STD:: to FT:: after ft::pair &less
+	class map {
+		public: 
+			typedef Key     										key_type;
+			typedef T												mapped_type;
+			// typedef ft::pair<key_type, mapped_type> 				value_type;
+			typedef Compare     									key_compare;
+			// value_compare	Nested function class to compare elements	see value_comp
+			typedef Alloc											allocator_type;
+			typedef typename allocator_type::reference				reference;
+			typedef typename allocator_type::const_reference		const_reference;
+			typedef typename allocator_type::pointer				pointer;
+			typedef typename allocator_type::const_pointer			const_pointer;
+			// typedef ft::bidirectional iterator<value_type>			iterator;
+			// typedef ft::bidirectional iterator <const value_type>	const_iterator;
+			// typedef ft::reverse_iterator<iterator>					reverse_iterator;
+			// typedef ft::reverse_iterator<const_iterator>			const_reverse_iterator;
+			typedef std::ptrdiff_t									difference_type;
+			typedef std::size_t										size_type;
+		private:
+			allocator_type  _alloc;
+			size_type		_size;
+	};
+}
