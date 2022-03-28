7d7612bad2d8889ed0438b059139bfce0438c777
nnieddu <nicolas.nieddu@free.fr>
2022-03-28 10:55:56 +0200

    add evil macro DUMP to mymain

--- mymain.cpp
+++ mymain.cpp
@@ -17,6 +17,14 @@
 
 // #include "fun.hpp"
 
+
+#define DUMPSTR_WNAME(os, name, a) \
+    do { (os) << (name) << " is value " << (a) << std::endl; } while(false)
+
+#define DUMPSTR(os, a) DUMPSTR_WNAME((os), #a, (a))
+#define DUMP(a)        DUMPSTR_WNAME(std::cout, #a, (a))
+
+
 int main ()
 {
 	// int myints[] = {16,2,77,29};
