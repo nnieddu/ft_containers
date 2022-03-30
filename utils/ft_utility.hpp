b07c1a6516f8e66896e38eed5032b666b599e4dc
nnieddu <nicolas.nieddu@free.fr>
2022-03-30 11:31:36 +0200

    put make_pair and pair in one utility header like stl

---
+++ utils/ft_utility.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utility.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ninieddu <ninieddu@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/30 11:30:25 by ninieddu          #+#    #+#             */
+/*   Updated: 2022/03/30 11:30:58 by ninieddu         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+// https://www.cplusplus.com/reference/utility/pair/?kw=pair
+
+// template <class T1, class T2> struct pair;
+
+
+// https://www.cplusplus.com/reference/utility/make_pair/?kw=make_pair
+
+// template <class T1, class T2>
+//   pair<T1,T2> make_pair (T1 x, T2 y);
