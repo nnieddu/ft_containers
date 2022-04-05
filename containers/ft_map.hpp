4bea5d8cef220bc22e082b6ef1a376d892a64ddf
nnieddu <nicolas.nieddu@free.fr>
2022-04-05 23:51:30 +0200

    add all proto and begin add basic functions

--- containers/ft_map.hpp
+++ containers/ft_map.hpp
@@ -45,121 +45,151 @@ namespace ft
 			typedef std::size_t											size_type;
 		private:	
 			allocator_type  _alloc;
+			size_type		_capacity;	
 			size_type		_size;
 		public: 
-		// Member functions
+		// [ Member functions ]
 
 		// (constructor)
-		//	Construct map (public member function )
+		// Construct map (public member function )
 
-		// (destructor)
-		//	Map destructor (public member function )
+		// empty (1)	
+		// explicit map (const key_compare& comp = key_compare(),
+			// const allocator_type& alloc = allocator_type());
 
-		// operator=
-		//	Copy container content (public member function )
+		// range (2)	
+		// template <class InputIterator>
+		// map (InputIterator first, InputIterator last,
+		// const key_compare& comp = key_compare(),
+		
+		// const allocator_type& alloc = allocator_type());
+
+		// copy (3)	
+		// map (const map& x);
+
+		// (destructor) : Map destructor (public member function )
+
+		// operator= : Copy container content (public member function )
+		// map& operator= (const map& x);
 
 
 		// Iterators:
 
 		// begin
-		//	Return iterator to beginning (public member function )
+		// Return iterator to beginning (public member function )
+		// iterator begin();
+
+		// const_iterator begin() const;
 
 		// end
-		//	Return iterator to end (public member function )
+		// Return iterator to end (public member function )
+		// iterator end();
+
+		// const_iterator end() const;
 
 		// rbegin
-		//	Return reverse iterator to reverse beginning (public member function )
+		// Return reverse iterator to reverse beginning (public member function )
+		// reverse_iterator rbegin();
+
+		// const_reverse_iterator rbegin() const;
 
 		// rend
-		//	Return reverse iterator to reverse end (public member function )
+		// Return reverse iterator to reverse end (public member function )
+		// reverse_iterator rend();
 
-		// cbegin
-		//	Return const_iterator to beginning (public member function )
-
-		// cend
-		//	Return const_iterator to end (public member function )
-
-		// crbegin
-		//	Return const_reverse_iterator to reverse beginning (public member function )
-
-		// crend
-		//	Return const_reverse_iterator to reverse end (public member function )
+		// const_reverse_iterator rend() const;
 
 
 		// Capacity:
 
 		// empty
-		//	Test whether container is empty (public member function )
+		// Test whether container is empty (public member function )
+		bool empty() const {return ( _size == 0 ? true : false); };
 
 		// size
-		//	Return container size (public member function )
+		// Return container size (public member function )
+		size_type size() const { return _size; }
 
 		// max_size
-		//	Return maximum size (public member function )
-
+		// Return maximum size (public member function )
+		size_type max_size() const { allocator_type().max_size(); }
 
 		// Element access:
 
 		// operator[]
-		//	Access element (public member function )
-
-		// at
-		//	Access element (public member function )
+		// Access element (public member function )
+		// mapped_type& operator[] (const key_type& k);
 
 
 		// Modifiers:
 
-		// insert
-		//	Insert elements (public member function )
+		// insert() : Insert elements (public member function )
+		// single element (1)	
+		// pair<iterator,bool> insert (const value_type& val);
 
-		// erase
-		//	Erase elements (public member function )
+		// with hint (2)	
+		// iterator insert (iterator position, const value_type& val);
 
-		// swap
-		//	Swap content (public member function )
+		// range (3)	
+		// template <class InputIterator>
+		// void insert (InputIterator first, InputIterator last);
 
-		// clear
-		//	Clear content (public member function )
+		// erase : Erase elements (public member function )
+		// (1)	
+		// 	void erase (iterator position);
 
-		// emplace
-		//	Construct and insert element (public member function )
+		// (2)	
+		// size_type erase (const key_type& k);
 
-		// emplace_hint
-		//	Construct and insert element with hint (public member function )
+		// (3)	
+		// 	void erase (iterator first, iterator last);
+
+		// swap : Swap content (public member function )
+		// void swap (map& x);
+
+		// clear : Clear content (public member function )
+		// void clear();
 
 
 		// Observers:
 
-		// key_comp
-		//	Return key comparison object (public member function )
+		// key_comp : Return key comparison object (public member function )
+		// key_compare key_comp() const;
 
-		// value_comp
-		//	Return value comparison object (public member function )
+		// value_comp : Return value comparison object (public member function )
+		// value_compare value_comp() const;
 
 
 		// Operations:
 
-		// find
-		//	Get iterator to element (public member function )
+		// find : Get iterator to element (public member function )
+		// iterator find (const key_type& k);
 
-		// count
-		//	Count elements with a specific key (public member function )
+		// const_iterator find (const key_type& k) const;
 
-		// lower_bound
-		//	Return iterator to lower bound (public member function )
+		// count : Count elements with a specific key (public member function )
+		// size_type count (const key_type& k) const;
 
-		// upper_bound
-		//	Return iterator to upper bound (public member function )
+		// lower_bound : Return iterator to lower bound (public member function )
+		// iterator lower_bound (const key_type& k);
 
-		// equal_range
-		//	Get range of equal elements (public member function )
+		// const_iterator lower_bound (const key_type& k) const;
+
+		// upper_bound : Return iterator to upper bound (public member function )
+		// iterator upper_bound (const key_type& k);
+
+		// const_iterator upper_bound (const key_type& k) const;
+
+		// equal_range : Get range of equal elements (public member function )
+		// pair<const_iterator,const_iterator> equal_range (const key_type& k) const;
+
+		// pair<iterator,iterator>	equal_range (const key_type& k);
 
 
 		// Allocator:
 
-		// get_allocator
-		//	Get allocator (public member function )
-
+		// get_allocator : Get allocator (public member function )
+		allocator_type get_allocator() const { return _alloc; }
 	};
 
 }
