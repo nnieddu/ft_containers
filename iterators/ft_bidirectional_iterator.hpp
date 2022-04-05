0a21613c695f6cfe2054263b4ffcfbe550798585
nnieddu <nicolas.nieddu@free.fr>
2022-04-05 17:25:31 +0200

    begin bidi

--- iterators/ft_bidirectional_iterator.hpp
+++ iterators/ft_bidirectional_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: ninieddu <ninieddu@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 13:02:19 by ninieddu          #+#    #+#             */
-/*   Updated: 2022/02/21 09:58:07 by ninieddu         ###   ########lyon.fr   */
+/*   Updated: 2022/04/05 15:24:57 by ninieddu         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,26 @@
 // https://www.cplusplus.com/reference/iterator/RandomAccessIterator/
 
 // Bidirectional iterators are like forward iterators but can also be iterated through backwards.
+// Is default-constructible, copy-constructible, copy-assignable and destructible	X a;
+// X b(a);
+// b = a;
+// Can be compared for equivalence using the equality/inequality operators
+// (meaningful when both iterator values iterate over the same underlying sequence).	a == b
+// a != b
+// Can be dereferenced as an rvalue (if in a dereferenceable state).	*a
+// a->m
+// For mutable iterators (non-constant iterators):
+// Can be dereferenced as an lvalue (if in a dereferenceable state).	*a = t
+// Can be incremented (if in a dereferenceable state).
+// The result is either also dereferenceable or a past-the-end iterator.
+// Two iterators that compare equal, keep comparing equal after being both increased.
+// ++a
+// a++
+// *a++
+// Can be decremented (if a dereferenceable iterator value precedes it).
+// --a
+// a--
+// *a--
 
 namespace ft
 {
