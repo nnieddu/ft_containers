5aceeb58e2f794f044ea735dd708f8c7d68a4d2b
nnieddu <nicolas.nieddu@free.fr>
2022-04-08 14:00:15 +0200

    add clean ft in destructor

--- utils/ft_rb_tree.hpp
+++ utils/ft_rb_tree.hpp
@@ -38,280 +38,284 @@ class rbtree
 
 		~rbtree()
 		{
+			if(root != nil)
+				clean(root);
+			delete root;
 			delete nil;
 		}
 	
 		node* nil;
 		node* root;
 
-	void insert(T key)
-	{
-		node* t = new node;
-		t->key = key;
-		node* x = root;
-		node* y = nil;
-		while(x != nil)
+		void insert(T key)
 		{
-			y = x;
-			if(key < x->key)
-				x = x->left;
-			else
-				x = x->right;
-		}
-		t->p = y;
-		if(y == nil)
-			root = t;
-		else
-		{
-			if (t->key < y->key)
-				y->left = t;
-			else
-				y->right = t;
-		}
-		t->left = nil;
-		t->right = nil;
-		t->color = true;
-		rbInsertFixup(t);
-	}
+			node* t = new node;
+			node* x = root;
+			node* y = nil;
 
-	node* search(T key)
-	{
-		node* x = root;
-		while(x != nil && key != x->key)
-		{
-			if(key < x->key)
-				x = x->left;
+			t->key = key;
+			while(x != nil)
+			{
+				y = x;
+				if(key < x->key)
+					x = x->left;
+				else
+					x = x->right;
+			}
+			t->p = y;
+			if(y == nil)
+				root = t;
 			else
-				x = x->right;
+			{
+				if (t->key < y->key)
+					y->left = t;
+				else
+					y->right = t;
+			}
+			t->left = nil;
+			t->right = nil;
+			t->color = true;
+			rbInsertFixup(t);
 		}
-		return x;
-	}
 
-	void erase(T key)
-	{
-		node* x = search(key);
-		if(x != nil)
-			rbDelete(x);
-	}
-
-	node* treeSuccessor(node* x)
-	{
-		if(x->right != nil)
+		node* search(T key)
 		{
-			while(x->left != nil)
-				x = x->left;
+			node* x = root;
+			while(x != nil && key != x->key)
+			{
+				if(key < x->key)
+					x = x->left;
+				else
+					x = x->right;
+			}
 			return x;
 		}
-		node* y = x->p;
-		while(y != nil && x == y->right)
-		{
-			x = y;
-			y = y->p;
-		}
-		return y;
-	}
 
-	void leftRotate(node*x)
-	{
-		node* y = x->right;
-		if(x->p == nil)
-			root = y;
-		else
+		void erase(T key)
 		{
-			if(x == x->p->left)
-				x->p->left = y;
-			else
-				x->p->right = y;
+			node* x = search(key);
+			if(x != nil)
+				rbDelete(x);
 		}
-		y->p = x->p;
-		x->right = y->left;
-		y->left->p = x;
-		y->left = x;
-		x->p = y;
-	}
 
-	void rightRotate(node*x)
-	{
-		node* y = x->left;
-		if(x->p == nil)
-			root = y;
-		else
+		node* treeSuccessor(node* x)
 		{
-			if(x->p->left == x)
-				x->p->left = y;
-			else
-				x->p->right = y;
-		}
-		y->p = x->p;
-		x->left = y->right;
-		y->right->p = x;
-		y->right = x;
-		x->p = y;
-	}
-
-	void rbInsertFixup(node*z)
-	{
-		while(z->p->color  == true)
-		{
-			if(z->p == z->p->p->left)
+			if(x->right != nil)
 			{
-				node* y = z->p->p->right;
-				if(y->color == true)
-				{
-					z->p->color = false;
-					y->color = false;
-					z->p->p->color = true;
-					z = z->p->p;
-				}
-				else
-				{
-					if(z == z->p->right)
-					{
-						z = z->p;
-						leftRotate(z);
-					}//
-					z->p->color = false;
-					z->p->p->color = true;
-					z->p->right->color = false;
-					rightRotate(z->p->p);
-				}
+				while(x->left != nil)
+					x = x->left;
+				return x;
 			}
+			node* y = x->p;
+			while(y != nil && x == y->right)
+			{
+				x = y;
+				y = y->p;
+			}
+			return y;
+		}
+
+		void leftRotate(node*x)
+		{
+			node* y = x->right;
+			if(x->p == nil)
+				root = y;
 			else
 			{
-				node* y = z->p->p->left;
-				if(y->color == true)
-				{
-					z->p->color = false;
-					y->color = false;
-					z->p->p->color = true;
-					z = z->p->p;
-				}
+				if(x == x->p->left)
+					x->p->left = y;
 				else
+					x->p->right = y;
+			}
+			y->p = x->p;
+			x->right = y->left;
+			y->left->p = x;
+			y->left = x;
+			x->p = y;
+		}
+
+		void rightRotate(node*x)
+		{
+			node* y = x->left;
+			if(x->p == nil)
+				root = y;
+			else
+			{
+				if(x->p->left == x)
+					x->p->left = y;
+				else
+					x->p->right = y;
+			}
+			y->p = x->p;
+			x->left = y->right;
+			y->right->p = x;
+			y->right = x;
+			x->p = y;
+		}
+
+		void rbInsertFixup(node* z)
+		{
+			while(z->p->color  == true)
+			{
+				if(z->p == z->p->p->left)
 				{
-					if(z == z->p->left)
+					node* y = z->p->p->right;
+					if(y->color == true)
 					{
-						z = z->p;
-						rightRotate(z);
+						z->p->color = false;
+						y->color = false;
+						z->p->p->color = true;
+						z = z->p->p;
 					}
-					z->p->color = false;
-					z->p->p->color = true;
-					z->p->left->color = false;
-					leftRotate(z->p->p);
-				}
-			}
-		}
-		root->color = false;
-	}
-
-	void rbDelete(node*z)
-	{
-		node* x = nil;
-		node* y = nil;
-
-		if(z->left == nil || z->right == nil)
-			y = z;
-		else
-			y = treeSuccessor(z);
-		if(y->left != nil)
-			x = y->left;
-		else
-			x = y->right;
-		x->p = y->p;
-		if(y->p == nil)
-			root = x;
-		else
-		{
-			if(y == y->p->left)
-				y->p->left = x;
-			else
-				y->p->right = x;
-		}
-		if(y != z)
-			z->key = y->key;
-		if(y->color == false)
-			rbDeleteFixup(x);
-		delete y;
-	}
-
-	void rbDeleteFixup(node* x)
-	{
-		while(x != root && x->color == false)
-		{
-			node* w = 0;
-
-			if(x->p->left == x)
-			{
-				w = x->p->right;
-				if(w->color == true)
-				{
-					w->color = false;
-					x->p->color = true;
-					leftRotate(x->p);
-					w = x->p->right;
-				}
-				if(w->left->color == false && w->right->color == false)
-				{
-					w->color = true;
-					x = x->p;
+					else
+					{
+						if(z == z->p->right)
+						{
+							z = z->p;
+							leftRotate(z);
+						}//
+						z->p->color = false;
+						z->p->p->color = true;
+						z->p->right->color = false;
+						rightRotate(z->p->p);
+					}
 				}
 				else
 				{
-					if(w->right->color == false)
+					node* y = z->p->p->left;
+					if(y->color == true)
 					{
-						w->left->color = false;
-						w->color = true;
-						rightRotate(w);
+						z->p->color = false;
+						y->color = false;
+						z->p->p->color = true;
+						z = z->p->p;
+					}
+					else
+					{
+						if(z == z->p->left)
+						{
+							z = z->p;
+							rightRotate(z);
+						}
+						z->p->color = false;
+						z->p->p->color = true;
+						z->p->left->color = false;
+						leftRotate(z->p->p);
+					}
+				}
+			}
+			root->color = false;
+		}
+
+		void rbDelete(node* z)
+		{
+			node* x = nil;
+			node* y = nil;
+
+			if(z->left == nil || z->right == nil)
+				y = z;
+			else
+				y = treeSuccessor(z);
+			if(y->left != nil)
+				x = y->left;
+			else
+				x = y->right;
+			x->p = y->p;
+			if(y->p == nil)
+				root = x;
+			else
+			{
+				if(y == y->p->left)
+					y->p->left = x;
+				else
+					y->p->right = x;
+			}
+			if(y != z)
+				z->key = y->key;
+			if(y->color == false)
+				rbDeleteFixup(x);
+			delete y;
+		}
+
+		void rbDeleteFixup(node* x)
+		{
+			while(x != root && x->color == false)
+			{
+				node* w = 0;
+
+				if(x->p->left == x)
+				{
+					w = x->p->right;
+					if(w->color == true)
+					{
+						w->color = false;
+						x->p->color = true;
+						leftRotate(x->p);
 						w = x->p->right;
 					}
-					w->color = x->p->color;
-					x->p->color = false;
-					w->right->color = false;
-					leftRotate(x->p);
-					x = root;
-				}
-			}
-			else
-			{
-				w = x->p->left;
-				if(w->color == true)
-				{
-					w->color = false;
-					x->p->color = true;
-					rightRotate(x->p);
-					w = x->p->left;
-				}
-				if(w->right->color == false && w->left->color == false)
-				{
-					w->color = true;
-					x = x->p;
+					if(w->left->color == false && w->right->color == false)
+					{
+						w->color = true;
+						x = x->p;
+					}
+					else
+					{
+						if(w->right->color == false)
+						{
+							w->left->color = false;
+							w->color = true;
+							rightRotate(w);
+							w = x->p->right;
+						}
+						w->color = x->p->color;
+						x->p->color = false;
+						w->right->color = false;
+						leftRotate(x->p);
+						x = root;
+					}
 				}
 				else
 				{
-					if(w->left->color == false)
+					w = x->p->left;
+					if(w->color == true)
 					{
-						w->right->color = false;
-						w->color = true;
-						leftRotate(w);
+						w->color = false;
+						x->p->color = true;
+						rightRotate(x->p);
 						w = x->p->left;
 					}
-					w->color = x->p->color;
-					x->p->color = false;
-					w->left->color = false;
-					rightRotate(x->p);
-					x = root;
+					if(w->right->color == false && w->left->color == false)
+					{
+						w->color = true;
+						x = x->p;
+					}
+					else
+					{
+						if(w->left->color == false)
+						{
+							w->right->color = false;
+							w->color = true;
+							leftRotate(w);
+							w = x->p->left;
+						}
+						w->color = x->p->color;
+						x->p->color = false;
+						w->left->color = false;
+						rightRotate(x->p);
+						x = root;
+					}
 				}
 			}
+			x->color = false;
 		}
-		x->color = false;
-	}
 
-	void display()
-	{
-		if(root != nil)
-			_display(root);
-		else
-			std::cout << "Tree is empty !" << std::endl;
-	}
+		void display()
+		{
+			if(root != nil)
+				_display(root);
+			else
+				std::cout << "Tree is empty !" << std::endl;
+		}
 
 		void _display(node* x)
 		{
@@ -344,6 +348,13 @@ class rbtree
 		
 		void clean(node* x)
 		{
-
+			if(x->right != nil)
+				clean(x->right);
+			if(x->right != nil)
+				delete x->right;
+			if(x->left != nil)
+				clean(x->left);
+			if(x->left != nil)
+				delete x->left;
 		}
 };
