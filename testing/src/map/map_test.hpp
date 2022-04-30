0dfefad96593666a5d2fb007eecd58d7fb7d5637
nnieddu <nicolas.nieddu@free.fr>
2022-04-30 14:33:39 +0200

    add testing

---
+++ testing/src/map/map_test.hpp
@@ -0,0 +1,20 @@
+#ifndef MAP_TEST_HPP
+# define MAP_TEST_HPP
+
+#include <iostream>
+#include "../../../containers/ft_map.hpp"
+
+#undef NAMESPACE
+#define NAMESPACE ft
+#ifdef STD_TEST
+	#undef NAMESPACE
+	#define NAMESPACE std
+#endif
+// NAMESPACE::vector<int> foo;
+
+void	map_test();
+void	map_iterator();
+void	map_const_iterator();
+void	map_reverse_iterator();
+
+#endif
